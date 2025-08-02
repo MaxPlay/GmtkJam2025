4bf289723717f49b384ba5d4eeaec1e8f62f9338
MaxPlay <maxplayyt@gmail.com>
2025-08-02 19:19:45 +0200

    Removed ApplyItem and moved the logic into Tick instead  -> This causes DOTween issues and the spawner is able to destory the current item Added Trash prefab Added TransformMachineModus  -> Can swap incoming items

--- GMTKJam2025/Assets/Game/Machines/MachineModus.cs
+++ GMTKJam2025/Assets/Game/Machines/MachineModus.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public abstract class MachineModus : MonoBehaviour
 {
-    public abstract ConveyorItem ApplyItem(ConveyorItem item);
-
     public virtual bool Tick(ConveyorItem currentItem, out ConveyorItem newItem)
     {
         newItem = null;
