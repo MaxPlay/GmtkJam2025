8d70a05d48a1a3ab078ac6181987b23081aafd52
MaxPlay <maxplayyt@gmail.com>
2025-08-02 12:03:26 +0200

    Added conveyor logic  -> Different types of conveyors have different implementation to accomodate for the movement of the goods  -> Setup prefabs  -> Added Coordinate logic that automatically links conveyors

--- GMTKJam2025/Assets/Game/Conveyor/Conveyor.cs
+++ GMTKJam2025/Assets/Game/Conveyor/Conveyor.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿using NaughtyAttributes;
+using System;
+using UnityEditor;
+using UnityEngine;
 
-public class Conveyor : MonoBehaviour
+public abstract class Conveyor : MonoBehaviour
 {
+    [SerializeField]
+    private ConveyorDirection direction;
+
+    [ReadOnly]
     [SerializeField]
     private Conveyor next;
 
+    [ReadOnly]
     [SerializeField]
     private Machine installedMachine;
 
@@ -12,4 +20,47 @@ public class Conveyor : MonoBehaviour
     {
 
     }
+
+    private void OnDrawGizmos()
+    {
+        Transform thisTransform = transform;
+        Vector3 directionVector = direction switch
+        {
+            ConveyorDirection.Forward => thisTransform.forward,
+            ConveyorDirection.Right => thisTransform.right,
+            ConveyorDirection.Back => -thisTransform.forward,
+            ConveyorDirection.Left => -thisTransform.right,
+            _ => throw new Exception("Invalid direction"),
+        };
+        DrawArrow.ForGizmo(thisTransform.position + Vector3.up, directionVector);
+
+        Handles.Label(thisTransform.position + Vector3.up * 1.1f, GameManager.CoordinateFromWorld(thisTransform.position).ToString()); 
+    }
+
+    public Vector2Int GetDeliveryCoordinate()
+    {
+        Transform thisTransform = transform;
+        Vector3 directionVector = direction switch
+        {
+            ConveyorDirection.Forward => thisTransform.forward,
+            ConveyorDirection.Right => thisTransform.right,
+            ConveyorDirection.Back => -thisTransform.forward,
+            ConveyorDirection.Left => -thisTransform.right,
+            _ => throw new Exception("Invalid direction"),
+        };
+        return GameManager.CoordinateFromWorld(thisTransform.position + directionVector * 2);
+    }
+
+    public void SetNext(Conveyor other)
+    {
+        next = other;
+    }
+}
+
+public enum ConveyorDirection
+{
+    Forward,
+    Right,
+    Back,
+    Left
 }
--- GMTKJam2025/Assets/Game/GameManager.cs
+++ GMTKJam2025/Assets/Game/GameManager.cs
@@ -7,7 +7,8 @@ public class GameManager : MonoBehaviour
     [ReadOnly]
     [SerializeField]
     private List<Conveyor> conveyors = new();
-    
+    private readonly Dictionary<Vector2Int, Conveyor> conveyorAtCoordinate = new();
+
     [ReadOnly]
     [SerializeField]
     private List<Machine> machines = new();
@@ -17,10 +18,29 @@ public class GameManager : MonoBehaviour
     private float tickDuration;
     private float lastTick;
 
+    public static Vector2Int CoordinateFromWorld(Vector3 position)
+        => new Vector2Int(Mathf.RoundToInt(position.x) - (position.x < 0.0f ? 1 : 0), Mathf.RoundToInt(position.z) - (position.z < 0.0f ? 1 : 0)) / 2;
+
     private void Start()
     {
-        conveyors.AddRange(FindObjectsByType<Conveyor>(FindObjectsInactive.Include, FindObjectsSortMode.None));
         machines.AddRange(FindObjectsByType<Machine>(FindObjectsInactive.Include, FindObjectsSortMode.None));
+
+        foreach (Conveyor conveyor in FindObjectsByType<Conveyor>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+        {
+            conveyors.Add(conveyor);
+            Vector2Int coordinate = CoordinateFromWorld(conveyor.transform.position);
+            Debug.Assert(!conveyorAtCoordinate.ContainsKey(coordinate), $"Multiple Conveyors at coordinate {coordinate}.");
+            conveyorAtCoordinate.Add(coordinate, conveyor);
+        }
+
+        foreach (Conveyor conveyor in conveyors)
+        {
+            Vector2Int next = conveyor.GetDeliveryCoordinate();
+            if (conveyorAtCoordinate.TryGetValue(next, out Conveyor other))
+            {
+                conveyor.SetNext(other);
+            }
+        }
     }
 
     private void Update()
